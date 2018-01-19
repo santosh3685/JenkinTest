edbd977f4e64bbf68adcaaac89cbe8d363bce223
santosh3685 <31529616+santosh3685@users.noreply.github.com>
2018-01-19 16:35:58 +0530

    Let to var

--- JenkiniOS/JenkiniOS/AppDelegate.swift
+++ JenkiniOS/JenkiniOS/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let x = 10
+        var x = 10
         x = 5
         return true
     }
