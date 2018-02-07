4811f30f681ddddfb12224d259115ddeeca5883e
Sami Karn <sami@INNO20PC02549.local>
2018-02-07 14:29:33 +0530

    Jenkins Test Commit

--- JenkiniOS/JenkiniOS/ViewController.swift
+++ JenkiniOS/JenkiniOS/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-        let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+        let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella", "Harry", "Santosh"]
         
         func backwards(s1: String, s2: String) -> Bool {
             return s1 > s2
